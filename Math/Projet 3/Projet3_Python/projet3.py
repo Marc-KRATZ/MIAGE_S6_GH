d5008899b7bfd33b3f9849bdd1313d284c8a5e77
Marc KRATZ <kratzmarc@gmail.com>
2018-04-14 20:09:37 +0200

    End math project

--- Math/Projet 3/Projet3_Python/projet3.py
+++ Math/Projet 3/Projet3_Python/projet3.py
@@ -1,61 +1,68 @@
 import os
 import sys
+import math
+import random
 
-
-# Classe générique pour toutes les surfaces 
+# Classe générique pour toutes les surfaces
 class Surface:
 
-    # la classe aura quatre attributs à instancier à la création des objets... 
-    # mais uniquement dans les classes qui en hérite 
+    # la classe aura quatre attributs à instancier à la création des objets...
+    # mais uniquement dans les classes qui en hérite
     # attributs : x_min, x_max, y_min, y_max
     # à initialiser avec des self.x_min = 0 par exemple
     # et à chaque appel, il faudra bien penser au self.x_min au lieu de x_min
     # ces nombres définissent le rectangle dans lequel les points seront tirés :
     # sommets : (x_min, y_min), (x_min, y_max), (x_max, y_max), (x_max, y_min)
-    
+
     def __init__(self):
         raise NotImplementedError("Vous ne pouvez pas instancier cette classe !")
 
     # tirage d'une abscisse au hasard dans l'intervalle [x_min; x_max]
     def randomX(self):
-        # TODO
-        return 0
-    
+        return (random.uniform(self.x_min,self.x_max))
+
     # tirage d'une ordonnée au hasard dans l'intervalle [y_min; y_max]
     def randomY(self):
-        # TODO
-        return 0
+        return (random.uniform(self.y_min,self.y_max))
 
     # permet de savoir si un point est dans la surface ou non :
     # à réimplémenter pour chaque type de surface (mais pas ici !)
     def isInArea(self,x,y):
         return true
-    
+
     # tirage de nbRepet points dans le rectangle de sommets :
     # (x_min, y_min), (x_min, y_max), (x_max, y_max), (x_max, y_min)
     # renvoie le nombre de ces points qui étaient dans la surface
     def countMonteCarlo(self,nbRepet):
-        # TODO
-        return 0
+        count = 0
+        i = 0
+        while i < nbRepet:
+            if (self.isInArea(self.randomX(),self.randomY())):
+                count = count + 1
+            i = i + 1
+            pass
+        return count
 
     # calcule une estimation de l'aire si count points étaient dans la surface
     # lors de nbRepet tirages de points
     def areaMonteCarlo(self,count,nbRepet):
-        # TODO
-        return 0
+        surf = (self.x_max-self.x_min) * (self.y_max-self.y_min)
+        return ((count * surf) / nbRepet)
 
     # calcule une estimation de l'aire si count points étaient dans la surface
     # lors de nbRepet tirages de points
     def printMonteCarlo(self,count,area):
-        # TODO
+        print(count,'{:.3f}'.format(area))
         return None
 
     # construit la chaîne de caractères avec en premier le nombre de points
-    # dans la surface, puis une espace, puis l'estimation de l'aire de la 
+    # dans la surface, puis une espace, puis l'estimation de l'aire de la
     # surface avec 3 chiffres obligatoires derrière la virgule
     # affiche cette chaîne de caractères
     def simulateMonteCarlo(self,nbRepet):
-        # TODO
+        count = self.countMonteCarlo(nbRepet)
+        area = self.areaMonteCarlo(count,nbRepet)
+        self.printMonteCarlo(count,area)
         return None
 
 # 1e experience : la surface est un rectangle
@@ -63,40 +70,58 @@ class Surface:
 # - celui dans lequel les points sont tirés (attributs d'une surface)
 # - celui que l'on cherche à mesurer (méthode isInArea)
 class Rectangle(Surface):
-    
+
     # définition du rectangle dans lequel les points seront tirés
     def __init__(self):
-        # TODO
+        self.x_min = 1
+        self.x_max = 4
+        self.y_min = 0.5
+        self.y_max = 2.5
         return None
-        
+
     # quand est-ce qu'un point tiré au hasard est dans le rectangle à mesurer ?
     def isInArea(self,x,y):
-        # TODO
-        return true
+        test = False
+        if(2<=x<=3):
+            if(1<=y<=2):
+                test = True
+        return test
 
 # 2e experience : la surface est un cercle
 class Circle(Surface):
 
     # définition du rectangle dans lequel les points seront tirés
     def __init__(self):
-        # TODO
+        self.x_min = -1
+        self.x_max = 1
+        self.y_min = -1
+        self.y_max = 1
         return None
 
     # quand est-ce qu'un point tiré au hasard est dans le cercle ?
     def isInArea(self,x,y):
-        # TODO
-        return true
+        test = False
+        if (math.sqrt((x*x)+(y*y))<=1):
+            test = True
+        return test
 
 # 3e experience : la surface est celle indiquée dans le graphique de l'énoncé
 class Graph(Surface):
-    
+
     def __init__(self):
-        # TODO
+        self.x_min = -1
+        self.x_max = 1
+        self.y_min = -2
+        self.y_max = 3
         return None
-    
+
     def isInArea(self,x,y):
-        # TODO
-        return true
+        test = False
+        if (math.exp(x)>=y):
+            #print((x-(x*x*x)-1),y)
+            if ((x-(x*x*x)-1)<=y):
+                test = True
+        return test
 
 if len(sys.argv) > 1:
     _nbRepet = int(sys.argv[1])
@@ -115,4 +140,3 @@ if len(sys.argv) > 1:
 
 else:
     print ("No arguments")
-
